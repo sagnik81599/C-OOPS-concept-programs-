8eec3ee6ebd7bb5cd4c4883865e17c6b9a7e8848
SAGNIK SAHA <126655769+sagnik81599@users.noreply.github.com>
2026-01-01 21:59:31 +0530

    Add files via upload

---
+++ problem_solve_qustion/convertNumber_decimal.cpp
@@ -0,0 +1,25 @@
+// Write a program that takes an integer input from the user and 
+// displays the number in decimal, octal, and hexadecimal formats using manipulator functions.
+
+// Note: This is a sample question asked in a TCS interview. 
+
+// Input format: The input consists of an integer N. Output format: 
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+
+int n;
+cout<<"Enter the number";
+cin>>n;
+
+
+
+cout<<"decimal no"<< dec <<n <<endl;
+cout<<"Octal no"<< oct <<n <<endl;
+
+
+
+}
